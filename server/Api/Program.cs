7affc238ac7dfd65b90fff97341121bf153d1045
Mohamed Ateto <mohamedhosny6757@gmail.com>
2026-08-06 20:26:29 +0300

    Fixing alot of things that i don't remember

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -30,7 +30,7 @@ namespace QuizesApi
             // Database context
             // Database context
             builder.Services.AddDbContext<ElsewedySchoolSysDbDevContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MySqlConnection")));
                 
             // Add Memory Cache for performance
             builder.Services.AddMemoryCache();
