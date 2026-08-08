0ccf383ef8f36afabc30e1369629f26a55f17bc0
Mohamed Ateto <mohamedhosny6757@gmail.com>
2026-08-08 15:45:21 +0300

    push updates finally !!

--- server/Api/obj/Debug/net9.0/QuizesApi.AssemblyInfo.cs
+++ server/Api/obj/Debug/net9.0/QuizesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4df5365da778e596507ad86f926301e843220b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427481013f54ac744b8cd0f7de5a356e8307a7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
